39d8c895d493ed3e08a58e7e36daa5db6225877e
vagrant <vagrant@homestead>
2016-03-02 14:22:49 +0000

    corrigiendo el error en las rutas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,11 +4,23 @@
 // "function" es una función anónima, sin nombre
 Route::get('/', function () {
     //return view('welcome');
-    //return view('home');
-    return "usando Git";
+    return view('home');
+    //return "usando Git";
 });
 
 Route::post('/form', 'ambientesController@show');
 
 //controlador
 Route::get('/controller', 'ambientesController@index');
+
+
+//acepta solo letras, no numeros
+Route::get('/nombre/{nombre}', function($nombre){
+if(preg_match("/^[A-Z a-z]+$/", $nombre)){
+	return "Mi nombre es: ".$nombre;
+
+}
+else{
+	return "no valido";
+}
+});
